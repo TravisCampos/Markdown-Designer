b6854e52d01a28d21599b9d304a5c4e8d8d5fb81
Travis Campos <traviscampos@Traviss-MBP.lan>
2020-08-31 21:19:54 -0700

    Node.js and Express

---
+++ express-backend/mongo-connection.js
@@ -0,0 +1,11 @@
+// From MongoDB Documentation linked in Project 3 Spec
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'BlogServer';
+
+{...}
---
+++ express-backend/routes/blog.js
@@ -0,0 +1,8 @@
+let express = require('express');
+let bcrypt = require('bcryptjs');
+let jwt = require('jsonwebtoken');
+
+let mongoConnection = require('../mongo-connection');
+const assert = require('assert');
+
+{...}
