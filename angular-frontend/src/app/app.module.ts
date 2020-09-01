db3f6266c1195af949fbca1b4cfd32c8a2a7c4a7
Travis Campos <traviscampos@Traviss-MBP.lan>
2020-08-31 21:18:20 -0700

    Angular

---
+++ angular-frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { EditComponent } from './edit/edit.component';
+import { PreviewComponent } from './preview/preview.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    EditComponent,
+    PreviewComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
